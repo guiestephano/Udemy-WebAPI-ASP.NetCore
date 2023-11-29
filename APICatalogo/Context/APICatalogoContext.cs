fa2cb07d6f57a89a0bfb8e00a9b22e60516f2bae
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2023-11-29 16:06:12 -0300

    Registrando os serviços na classe Program

--- APICatalogo/Context/APICatalogoContext.cs
+++ APICatalogo/Context/APICatalogoContext.cs
@@ -1,6 +1,14 @@
-﻿namespace APICatalogo.Context
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class APICatalogoContext : DbContext
 {
-    public class APICatalogoContext
+    public APICatalogoContext(DbContextOptions<APICatalogoContext> options) : base (options)
     {
     }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
 }
