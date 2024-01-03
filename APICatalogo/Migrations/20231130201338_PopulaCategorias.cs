283b608abdd59218dddb32c9385987c7d86ab5a4
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-01-03 02:53:44 -0300

    Populando Banco de Dados

---
+++ APICatalogo/Migrations/20231130201338_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas','Bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches','Lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas','Sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20240103053551_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" +
+                "VALUES ('Coca-Cola Diet','Refrigerante de Cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" +
+                "VALUES ('Lanche de Atum','Lanche de Atum com maionese',8.50,'lancheatum.jpg',10,now(),2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" +
+                "VALUES ('Pudim 100g','Pudim de Leite Condensado 100g',6.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
