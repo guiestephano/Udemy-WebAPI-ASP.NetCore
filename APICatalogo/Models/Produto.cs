61b84ce5053dafe80c535cb069e61dd3d80922dc
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-20 23:43:09 -0300

    Serializacao e Desserializacao JSON

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
@@ -27,5 +28,6 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaID { get; set; }
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
