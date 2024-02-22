f0723ac49021d7da7488aa4e28b4ba3729622179
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-22 01:09:26 -0300

    Minimal API MapGet e MapPost

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDb"));
 
 var app = builder.Build();
 
@@ -16,4 +20,27 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Olá Mundo!");
 
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+});
 app.Run();
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
